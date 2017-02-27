3d1300fe7a2af32d8bc0eef0097d9645b0784805
dty <dty@kamstrup.com>
2017-02-27 13:04:48 +0100

    Add initial project files

---
+++ Postkassedims/Postkassedims/main.c
@@ -0,0 +1,18 @@
+/*
+ * Postkassedims.c
+ *
+ * Created: 27-02-2017 13:01:08
+ * Author : dty
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
