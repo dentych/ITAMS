030a6070aa9ffefd4e6fa88e01768c3f9a743b3d
dty <dty@kamstrup.com>
2017-03-06 08:52:33 +0100

    Add ADC files

---
+++ Postkassedims/PostDims/ADC.c
@@ -0,0 +1,57 @@
+/*
+ * ADC.c
+ *
+ * Created: 03-03-2017 11:58:27
+ *  Author: dty
+ */ 
+
+#define F_CPU 3868400
+#include <avr/io.h>
+#include <util/delay.h>
+
+void InitADC() {
+	ADMUX = 0b01000000;
+	ADMUX |= (1<<ADLAR);
+	
+	// Enable ADC and set prescaler to 32.
+	ADCSRA = 0b10000101;
+}
+
+int16_t ReadADC() {
+	ADCSRA |= (1<<ADSC);
+	
+	PORTB &= ~(1<<6);
+	WaitForConversionFinish();
+	PORTB |= (1<<6);
+	
+	return ADCH;
+}
+
+void WaitForConversionFinish() {
+	while ((ADCSRA & (1<<ADSC)) != 0)
+	{
+	}
+}
+
+void TestADC() {
+	DDRB = 0xFF;
+	DDRD = 0xFF;
+	PORTD = 0xFF;
+	PORTB = 0xFF;
+
+	InitADC();
+
+	while (1)
+	{
+		PORTB &= ~(0b00000001);
+		int16_t data = ReadADC();
+		_delay_ms(50);
+		PORTB |= 0b00000001;
+		if (data > 128) {
+			PORTB &= ~(1<<7);
+		} else {
+			PORTB |= (1<<7);
+		}
+		_delay_ms(100);
+	}
+}
---
+++ Postkassedims/PostDims/ADC.h
@@ -0,0 +1,16 @@
+/*
+ * ADC.h
+ *
+ * Created: 03-03-2017 11:58:37
+ *  Author: dty
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+void InitADC();
+int ReadADC();
+void TestADC();
+
+#endif /* ADC_H_ */
